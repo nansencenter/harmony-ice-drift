bb87dc9775a52b17470ef5cd630e752fab6e89e1
akorosov <korosov@gmail.com>
2023-01-19 15:04:40 +0100

    add 2023/01_one_scene_processing.ipynb

--- 2023/harmony23lib.py
+++ 2023/harmony23lib.py
@@ -1,7 +1,8 @@
 import numpy as np
-from scipy.ndimage import zoom, median_filter
+from scipy.ndimage import zoom, median_filter, gaussian_filter, distance_transform_edt, minimum_filter
 from scipy.interpolate import griddata
 from matplotlib.tri import Triangulation, LinearTriInterpolator
+from stereoid.sea_ice import RadarModel
 
 def read_nextsim_data(filename, proj, lon_0=0):
     """
@@ -120,3 +121,215 @@ def compute_nextsim_uv(v_e_sz, v_n_sz, landmask, nor_sz, med_filt_size=7):
     v_x_sz[landmask] = np.nan
     v_y_sz[landmask] = np.nan
     return v_x_sz, v_y_sz
+
+
+def get_doppler(sgm, obs_geo, u_int, v_int, x_res, pol, inc_m, fstr_s1, fstr_ati, prod_res, b_ati):
+    """
+    Get radar observed Doppler. [From "E2E nExtSIM to Harmony observed drift velocities.ipynb"]
+    """
+    x_range_vec = np.arange(u_int.shape[1]) * x_res
+    nrcs, dopp = sgm.run_forward_model(obs_geo, pol, u_int, v_int, x_range_vec, inc_m) # NRCS and signal Doppler
+    obs_geo.concordia.set_swath(inc_m, x_range_vec)
+    obs_geo.discordia.set_swath(inc_m, x_range_vec)
+    radarm = RadarModel(obs_geo.concordia, fstr_s1, fstr_ati, fstr_ati, prod_res=prod_res, b_ati=b_ati)
+    s_dopp = radarm.sigma_dop(10 ** (nrcs/10)) # Doppler noise standard deviation
+    s_dopp_rnd = s_dopp*np.random.randn(dopp.shape[0],dopp.shape[1],3) # NESZ (profile + speckle)
+    r_dopp_r0 = dopp + s_dopp_rnd # radar observed Doppler
+    
+    return dopp, s_dopp, r_dopp_r0
+
+def fill_nan_gaps(array, mask, distance=5):
+    dist, indi = distance_transform_edt(
+        mask,
+        return_distances=True,
+        return_indices=True)
+    gpi = dist <= distance
+    r,c = indi[:,gpi]
+    array2 = np.array(array)
+    array2[gpi] = array[r,c]
+    return array2
+
+def remove_texture_noise(r_dopp, s_dopp, noise_factor=0.7, gf_size=10):
+    """ Remove texture noise [Park et al., 2019] """
+    noise = s_dopp - s_dopp.min(axis=(0,1))
+    noise /= noise.max(axis=(0,1))
+    noise *= 0.7
+    r_dopp_d = np.array(r_dopp)
+    for i in range(3):
+        r_dopp_i = r_dopp[:,:,i]
+        r_dopp_f = fill_nan_gaps(r_dopp_i, np.isnan(r_dopp_i), 200)
+        r_dopp_g = gaussian_filter(r_dopp_f, gf_size, truncate=2)
+        r_dopp_g[np.isnan(r_dopp_i)] = np.nan
+        r_dopp_d[:,:,i] = r_dopp_g*noise[:,:,i] + r_dopp_i*(1 - noise[:,:,i])
+
+    return r_dopp_d
+
+def get_power_spectrum_mk(v, m=10):
+    """ Get power spectrum [Kleinherenbrink et al., 2021] """
+     # split so we can do some kind of Bartlett in the along-track direction
+    shp=v.shape
+    l=int(np.floor(shp[0]/m))
+    Pv = np.zeros(shp)
+    #Pun=np.zeros(shp); Pvn=np.zeros(shp)
+    for i in range(0, m):
+        # make sure the splitted data has the same size (zero-padding)
+        v_temp=np.zeros(shp)
+        # split data
+        v_temp[i*l:(i+1)*l,:] = v[i*l:(i+1)*l,:]
+        # periodograms
+        PSD_v = np.absolute(np.fft.fft2(v_temp))**2/l/shp[1]
+        # mean periodogram
+        Pv += PSD_v
+    for i in range(0,m-1):
+        # make sure the splitted data has the same size (zero-padding)
+        v_temp=np.zeros(shp)
+        # split data
+        v_temp[int(i+0.5)*l:(int(i+0.5)+1)*l,:]=v[int(i+0.5)*l:(int(i+0.5)+1)*l,:]
+        # periodograms
+        PSD_v=np.absolute(np.fft.fft2(v_temp))**2/l/shp[1]
+        # mean periodogram
+        Pv=Pv+PSD_v
+    Pv = Pv/(2*m-1)
+    return Pv
+
+def denoise_mk(v1, v2, alpha=0.5):
+    """ Remove high frequency noise [Kleinherenbrink et al., 2021] """
+    v1c = v1 - np.mean(v1)
+    v2c = v2 - np.mean(v2)
+    vn = v2c - v1c
+    Pv2 = get_power_spectrum_mk(v2c)
+    Pve = Pv2 - np.std(vn)**2*alpha
+    Pve[Pve < 0] = np.nan
+    V2 = np.fft.fft2(v2c)
+    Pve[np.isnan(Pve)] = 0
+    v2fc = np.real(np.fft.ifft2(Pve / Pv2 * V2))
+    v2f = v2fc + np.mean(v2)
+    return v2f
+
+def apply_anisotropic_diffusion(img, gamma=0.25, step=(1., 1.), ploton=False, kappa = 50, niter = 10, option = 2):
+    """
+    Anisotropic diffusion.
+
+    Usage:
+    imgout = anisodiff(im, niter, kappa, gamma, option)
+
+    Arguments:
+            img    - input image
+            niter  - number of iterations
+            kappa  - conduction coefficient 20-100 ?
+            gamma  - max value of .25 for stability
+            step   - tuple, the distance between adjacent pixels in (y,x)
+            option - 1 Perona Malik diffusion equation No 1
+                     2 Perona Malik diffusion equation No 2
+            ploton - if True, the image will be plotted on every iteration
+
+    Returns:
+            imgout   - diffused image.
+
+    kappa controls conduction as a function of gradient.  If kappa is low
+    small intensity gradients are able to block conduction and hence diffusion
+    across step edges.  A large value reduces the influence of intensity
+    gradients on conduction.
+
+    gamma controls speed of diffusion (you usually want it at a maximum of
+    0.25)
+
+    step is used to scale the gradients in case the spacing between adjacent
+    pixels differs in the x and y axes
+
+    Diffusion equation 1 favours high contrast edges over low contrast ones.
+    Diffusion equation 2 favours wide regions over smaller ones.
+
+    Reference:
+    P. Perona and J. Malik.
+    Scale-space and edge detection using ansotropic diffusion.
+    IEEE Transactions on Pattern Analysis and Machine Intelligence,
+    12(7):629-639, July 1990.
+
+    Original MATLAB code by Peter Kovesi
+    School of Computer Science & Software Engineering
+    The University of Western Australia
+    pk @ csse uwa edu au
+    <http://www.csse.uwa.edu.au>
+
+    Translated to Python and optimised by Alistair Muldal
+
+    Sep 2017 modified by Denis Demchev
+    """
+
+    # init args
+    
+    # Conduction coefficient
+    
+    
+    # Number of iterations
+    
+    
+    # Number of equation (1,2)
+
+    if img.ndim == 3:
+        warnings.warn("Only grayscale images allowed, converting to 2D matrix")
+        img = img.mean(2)
+
+    # initialize output array
+    img = img.astype('float32')
+    imgout = img.copy()
+
+    # initialize some internal variables
+    deltaS = np.zeros_like(imgout)
+    deltaE = deltaS.copy()
+    NS = deltaS.copy()
+    EW = deltaS.copy()
+    gS = np.ones_like(imgout)
+    gE = gS.copy()
+
+    # create the plot figure, if requested
+    if ploton:
+        import pylab as pl
+
+        fig = pl.figure(figsize=(20, 5.5), num="Anisotropic diffusion")
+        ax1, ax2 = fig.add_subplot(1, 2, 1), fig.add_subplot(1, 2, 2)
+
+        ax1.imshow(img, interpolation='nearest')
+        ih = ax2.imshow(imgout, interpolation='nearest', animated=True)
+        ax1.set_title("Original image")
+        ax2.set_title("Iteration 0")
+
+        fig.canvas.draw()
+
+    for ii in range(niter):
+
+        # calculate the diffs
+        deltaS[:-1, :] = np.diff(imgout, axis=0)
+        deltaE[:, :-1] = np.diff(imgout, axis=1)
+
+        # conduction gradients (only need to compute one per dim!)
+        if option == 1:
+            gS = np.exp(-(deltaS / kappa) ** 2.) / step[0]
+            gE = np.exp(-(deltaE / kappa) ** 2.) / step[1]
+        elif option == 2:
+            gS = 1. / (1. + (deltaS / kappa) ** 2.) / step[0]
+            gE = 1. / (1. + (deltaE / kappa) ** 2.) / step[1]
+
+        # update matrices
+        E = gE * deltaE
+        S = gS * deltaS
+
+        # subtract a copy that has been shifted 'North/West' by one
+        # pixel. don't as questions. just do it. trust me.
+        NS[:] = S
+        EW[:] = E
+        NS[1:, :] -= S[:-1, :]
+        EW[:, 1:] -= E[:, :-1]
+
+        # update the image
+        imgout += gamma * (NS + EW)
+
+        if ploton:
+            iterstring = "Iteration %i" % (ii + 1)
+            ih.set_data(imgout)
+            ax2.set_title(iterstring)
+            fig.canvas.draw()
+            # sleep(0.01)
+
+    return imgout
