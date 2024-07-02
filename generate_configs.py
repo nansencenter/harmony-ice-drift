4a52939d3ed97a11641d6e776cb22fa2ee68cd30
Anton Korosov <anton.korosov@nersc.no>
2024-07-02 16:21:44 +0200

    many changes

---
+++ generate_configs.py
@@ -0,0 +1,9 @@
+
+conf_dir = '/Home/antonk/n/nextsim-env/config_files'
+exp_config_template = f'{conf_dir}/sa10_exp_template.cfg'
+
+
+params = {
+    'dynamics.C_lab': [0.02, 0.1],
+    'dynamics.compression_factor': [0, 0.5, 1],
+}
